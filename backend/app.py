32eefbff4170df590985b744c864dc809cf8830c
Anirudh Rao <ani.rao20@gmail.com>
2023-11-29 09:07:11 -0800

    Initial commit, client-server-database connection is working.
    Please enter the commit message for your changes. Lines starting
    with '#' will be ignored, and an empty message aborts the commit.
    
    Date:      Wed Nov 29 09:07:11 2023 -0800
    
    On branch main
    
    Initial commit
    
    Changes to be committed:
    	new file:   .idea/.gitignore
    	new file:   .idea/PotteryInventoryBackend.iml
    	new file:   .idea/inspectionProfiles/profiles_settings.xml
    	new file:   .idea/misc.xml
    	new file:   .idea/modules.xml
    	new file:   .idea/vcs.xml
    	new file:   backend/app.py

---
+++ backend/app.py
@@ -0,0 +1,68 @@
+from flask import Flask, jsonify, request
+import mysql.connector
+
+app = Flask(__name__)
+
+cnx = mysql.connector.connect(user='root', password='Th1s1sMYSQLPASS',
+                              host='127.0.0.1',
+                              database='PotteryInventory')
+
+
+@app.route('/api/data/retrieve', methods=['POST', 'GET'])
+def retrieve_data():
+    try:
+        crsr = cnx.cursor(dictionary=True)
+        query = request.data.decode()
+        query = eval(query)
+        searchTerm = query['searchTerm']
+        colName = query['colName']
+        q = ""
+        if (colName == 'Item_ID') or (colName == 'Price'):
+            q = "SELECT * FROM Inventory WHERE {} = {}".format(colName, searchTerm)
+        else:
+            q = "SELECT * FROM Inventory WHERE {} = {!r}".format(colName, searchTerm)
+        print(q)
+        crsr.execute(q)
+        result_list = []
+        for entry in crsr:
+            result_list.append(entry)
+            print(entry)
+        j_res = jsonify(result_list)
+        crsr.close()
+        print("Query complete.")
+        return j_res
+    except mysql.connector.Error as err:
+        err_str = "Something went wrong: {}".format(err)
+        err_resp = {"err": True, "resp": err_str}
+        return jsonify(err_resp)
+
+
+@app.route('/api/data/create', methods=['POST', 'GET'])
+def create_data():
+    try:
+        crsr = cnx.cursor(dictionary=True)
+        query = request.data.decode()
+        print(query)
+        query = eval(query)
+        print(query['Color'])
+        q = ("INSERT INTO Inventory (Color, Type, Price, Description)"
+             " VALUES ({!r}, {!r}, {!r}, {!r});").format(query['Color'], query['Type'], query['Price'],
+                                                         query['Description'])
+        print(q)
+        crsr.execute(q)
+        cnx.commit()
+        crsr.execute("SELECT * FROM Inventory WHERE Item_ID = (SELECT LAST_INSERT_ID());")
+        result_list = []
+        for entry in crsr:
+            result_list.append(entry)
+            print(entry)
+        j_res = jsonify(result_list)
+        crsr.close()
+        return j_res
+    except mysql.connector.Error as err:
+        err_str = "Something went wrong: {}".format(err)
+        return jsonify(err_str)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)

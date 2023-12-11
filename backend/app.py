436556d315f13d7ec90f01f1bab5265326c9c8ee
anirao20 <ani.rao20@gmail.com>
2023-12-11 12:38:48 -0800

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,7 @@ import mysql.connector
 
 app = Flask(__name__)
 
-cnx = mysql.connector.connect(user='root', password='Th1s1sMYSQLPASS',
+cnx = mysql.connector.connect(user='root', password='',
                               host='127.0.0.1',
                               database='PotteryInventory')
 
